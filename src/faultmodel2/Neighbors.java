5835e4f58cbee6e3461d8d4b38db6125fa08b236
Jingyu Yao <yjy.emails@gmail.com>
2013-04-21 17:37:45 -0400

    Start of project This is version 2 of the FaultModel project

---
+++ src/faultmodel2/Neighbors.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faultmodel2;
+
+/**
+ *
+ * @author Jingyu Yao
+ */
+public class Neighbors {
+    
+}
